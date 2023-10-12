3e21ec6707161a249bcdd433f5e4a53a500fbfb7
mathieu.dehan <mathieu.dehan@epitech.eu>
2023-10-12 15:25:41 +0200

    first test nextjs

---
+++ frontend/app/page.js
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import Navbar from "@/components/navigation/Navbar";
+
+export default function Home() {
+  return (
+    <main className="">
+      <Navbar />
+    </main>
+  );
+}
---
+++ frontend/components/navigation/Navbar.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <>
+      <h1 className="text-xl">Jobly</h1>
+    </>
+  );
+}
