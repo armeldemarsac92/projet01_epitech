8a64112f2477092ee9cf3d32bce1698cb9abbb34
armeldemarsac92 <armeldemarsac@gmail.com>
2023-10-13 18:30:06 +0200

    nextjs components create Footer.js

--- backend/src/Entity/ProfessionnalExperience.php
+++ backend/src/Entity/ProfessionnalExperience.php
@@ -2,14 +2,14 @@
 
 namespace App\Entity;
 
-use App\Repository\ProfesionnalExperienceRepository;
+use App\Repository\ProfessionnalExperienceRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: ProfesionnalExperienceRepository::class)]
-class ProfesionnalExperience
+#[ORM\Entity(repositoryClass: ProfessionnalExperienceRepository::class)]
+class ProfessionnalExperience
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
