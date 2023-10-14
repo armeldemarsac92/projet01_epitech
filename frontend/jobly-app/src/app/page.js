89d5664035c71ca37bf04ef4de62d1db0a5d5af5
armeldemarsac92 <armeldemarsac@gmail.com>
2023-10-14 13:12:46 +0200

    nextjs app edit page.js

--- frontend/jobly-app/src/app/page.js
+++ frontend/jobly-app/src/app/page.js
@@ -1,16 +1,28 @@
-import Image from 'next/image'
+'use client'
 import Footer from './components/Footer'
+import './styles/global.css'
+import { useState } from 'react';
 
 export default function Home() {
+
+  const [mode, setMode] = useState("candidate");
+
+  const userStyles = mode === "candidate" ?
+    'bg-gradient-candidate':
+    'bg-gradient-recruiter';
+
   return (
-    <main className='flex flex-row w-full justify-center'>
-      <div className='flex flex-col w-1/2 h-screen'>
-        <div className='flex flex-col h-1/3'></div>
-        <div className='flex flex-col h-1/3 flex-start items-center'>
-          <h1 className='font-semibold text-3xl'>Find your dream job</h1>
+    <main className={userStyles}>
+      <div className='flex flex-row w-full justify-center'>
+        <div className='flex flex-col w-1/2 h-screen'>
+          <div className='flex flex-col h-1/3'></div>
+          <div className='flex flex-col h-1/3 flex-start items-center'>
+            <h1 className='font-semibold text-5xl'>Find your dream job</h1>
+          </div>
+          <div className='flex flex-col h-1/3'></div>
         </div>
-        <div className='flex flex-col h-1/3'></div>
       </div>
+      <Footer></Footer>
     </main>
   )
 }
