31ef445148e613f7dbc309814f36d54c1fd2e67c
mathieu.dehan <mathieu.dehan@epitech.eu>
2023-10-16 13:40:57 +0200

    nextjs app edit header

--- frontend/jobly-app/src/app/page.js
+++ frontend/jobly-app/src/app/page.js
@@ -1,34 +1,31 @@
-'use client'
-import Footer from './components/Footer'
-import Search from './components/Search';
-import './styles/global.css'
-import { useState } from 'react';
+"use client";
+import Footer from "./components/Footer";
+import Search from "./components/Search";
+import "./styles/global.css";
+import { useState } from "react";
 
 export default function Home() {
-
   const [mode, setMode] = useState("candidate");
 
-  const userStyles = mode === "candidate" ?
-    'bg-gradient-candidate':
-    'bg-gradient-recruiter';
+  const userStyles =
+    mode === "candidate" ? "bg-gradient-candidate" : "bg-gradient-recruiter";
 
   return (
     <main className={userStyles}>
-      <div className='flex flex-row w-full justify-center'>
-        <div className='flex flex-col w-1/2 h-screen'>
-          <div className='flex flex-col h-1/3'></div>
-          <div className='flex flex-col h-1/3'>
-            <div className='flex flex-col h-fit flex-start items-center'>
-              <h1 className='font-semibold text-3xl'>Find your dream job</h1>
+      <div className="flex flex-row w-full justify-center">
+        <div className="flex flex-col w-1/2 h-screen">
+          <div className="flex flex-col h-1/3"></div>
+          <div className="flex flex-col h-1/3">
+            <div className="flex flex-col h-fit flex-start items-center">
+              <h1 className="font-semibold text-3xl">Find your dream job</h1>
             </div>
-            <div className='flex flex-col h-2/3 items-center'>
+            <div className="flex flex-col h-2/3 items-center">
               <Search></Search>
             </div>
           </div>
-          <div className='flex flex-col h-1/3'></div>
+          <div className="flex flex-col h-1/3"></div>
         </div>
       </div>
-      <Footer></Footer>
     </main>
-  )
+  );
 }
