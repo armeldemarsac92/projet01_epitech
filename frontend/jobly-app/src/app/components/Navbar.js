e912049d0526a5aca4fdfb06ea6980e9fcc93a2e
armeldemarsac92 <armeldemarsac@gmail.com>
2023-10-18 09:05:31 +0200

    NextJS app edit navbar

--- frontend/jobly-app/src/app/components/Navbar.js
+++ frontend/jobly-app/src/app/components/Navbar.js
@@ -2,13 +2,16 @@
 
 import React from "react";
 import CustomSwitch from "./Switch";
+import Link from "next/link";
 
 export default function Navbar({ onToggle, mode }) {
   return (
       <header className="fixed top-0 w-full text-gray-700 bg-white shadow-sm body-font z-50">
           <nav className="justify-between flex flex-col items-center p-6 mx-auto md:flex-row">
               <a className="flex items-center mb-4 font-medium text-gray-900 title-font md:mb-0">
-                  <img className="w-auto h-12" src="/logo.svg" alt="Logo" />
+                <Link href={"/"}>
+                <img className="w-auto h-12" src="/logo.svg" alt="Logo" />
+                </Link>
               </a>
               <div className="h-full pl-6 ml-6 border-l border-gray-200 flex flex-row items-center">
                   <a href="#_" className="mr-5 font-medium hover:text-gray-900">
