1633a2e6ea421f33c9bfb4bafb59f99774e7d5cd
armeldemarsac92 <armeldemarsac@gmail.com>
2023-10-18 22:19:43 +0200

    nexjs admin edit dashboard

--- frontend/jobly-app/src/app/admin/dashboard/page.js
+++ frontend/jobly-app/src/app/admin/dashboard/page.js
@@ -2,11 +2,11 @@
 import { useEffect, useState } from "react";
 import "../../styles/global.css";
 import Axios from "axios";
-import Navbar from "../../components/Navbar";
 
 export default function AdminDashBoard() {
   const [tables, setTables] = useState([]);
   const [table, setTable] = useState([]);
+  const [asideOpen, setAsideOpen] = useState(true);
 
   useEffect(() => {
     const fetchTablesUrl = "http://localhost:8000/api/get/database_tables";
@@ -31,54 +31,111 @@ export default function AdminDashBoard() {
       .catch((error) => {
         console.error("Error fetching data", error);
       });
-
-    const displayTable = (table) => {
-      i = 0;
-      return ({table.map((line) => {
-      
-        if ( i === 0 ) {
-          <thead class="block md:table-header-group">
-            <tr class="border border-grey-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto  md:relative ">
-            {line.map((columnHeader) => {
-              <th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">
-                {columnHeader}
-              </th>;
-            })}
-            </tr>
-        </thead>;
-      }
-      })})
-        
-    }
     
   };
 
+  const displayTable = (table) => {
+    if (!table || table.length === 0) return null;
   
+    return (
+      <div className={`${table.length > 19 ? "w-full h-full overflow-x-scroll overflow-y-scroll":"w-full h-fit"} rounded-lg border border-gray-200 shadow-md`}>
+        <table className="table-auto w-full border-collapse bg-white text-left text-sm text-gray-500 border-gray-200 rounded-lg">
+          <thead className="bg-gray-50">
+            <tr>
+              {Object.keys(table[0]).map((header) => (
+                <th key={header} scope="col" className="px-6 py-4 font-medium text-gray-900">
+                  {header}
+                </th>
+              ))}
+              <th scope="col" className="px-6 py-4 font-medium text-gray-900">Actions</th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-gray-100 border-t border-gray-100">
+            {table.map((row, rowIndex) => (
+              <tr key={rowIndex} className="hover:bg-gray-50">
+                {Object.values(row).map((cell, cellIndex) => (
+                  <td key={cellIndex} className="px-6 py-4">
+                    {cell}
+                  </td>
+                ))}
+                <td className="px-6 py-4">
+                  <div className="flex justify-end gap-4">
+                    <a x-data="{ tooltip: 'Delete' }" href="#">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="h-6 w-6"
+                        x-tooltip="tooltip"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                        />
+                      </svg>
+                    </a>
+                    <a x-data="{ tooltip: 'Edit' }" href="#">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="h-6 w-6"
+                        x-tooltip="tooltip"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+                        />
+                      </svg>
+                    </a>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 
   return (
-    <main>
-      <div className="flex flex-box">
-        <div className="bg-admin w-1/3 p-10">
-          <nav className="flex flex-col h-screen max-h-screen overflow-scroll border-2 border-white rounded-md p-10">
-            <h1 className="text-white text-xl pt-2 pl-1">DashBoard menu</h1>
-            <ul className="flex flex-col">
-              {tables.map((tableName) => (
-                <li
-                  onClick={() => loadTable(tableName.name)}
-                  className="flex flex-row border-2 border-white p-4 m-2 text-white rounded-md"
-                  key={tableName.name}
-                >
-                  <p>{tableName.name}</p>
-                  <p>{tableName.row_count}</p>
-                </li>
-              ))}
-            </ul>
-          </nav>
+<main className="min-h-screen w-full bg-gray-100 text-gray-700">
+      <header className="flex w-full items-center justify-between border-b-2 border-gray-200 bg-white p-2">
+        <div className="flex items-center space-x-2">
+          <button className="text-3xl" onClick={() => setAsideOpen(!asideOpen)}>
+            <i className="bx bx-menu"></i>
+          </button>
+          <div><img className="w-auto h-12" src="/logo.svg" alt="Logo" /></div>
         </div>
-        <div className="bg-gray-200 h-screen w-2/3">
-            
+      </header>
+
+      <div className="flex h-screen ">
+        <aside className={`h-screen overflow-y-scroll flex w-72 flex-col space-y-2 border-r-2 border-gray-200 bg-white p-2 ${asideOpen ? '' : 'hidden'}`}>
+          {tables.map((tableName) => (
+            <a
+              key={tableName.name}
+              href="#"
+              onClick={() => loadTable(tableName.name)}
+              className="flex items-center space-x-1 rounded-md px-2 py-3 hover:bg-gray-100 hover:text-blue-600"
+            >
+              <span className="text-2xl">
+                <i className="bx bx-table"></i>
+              </span>
+              <span>{tableName.name}</span>
+            </a>
+          ))}
+        </aside>
+        <div className="bg-gray-200 w-full h-screen flex justify-center items-start p-10">
+        {displayTable(table)}
         </div>
       </div>
     </main>
   );
 }
+
